41ce89a14538c4a43f87f28bb6a56851e6ec69fd
Imran Ahmed <imran.ahk26@gmail.com>
2025-07-13 22:14:44 +0600

    :fire: minor tweak to fix toggle button flicker issue. didnt fix though

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { ThemeProvider } from 'next-themes';
+import React from "react";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <div className='bg-[#f5f5f5] text-gray-700 dark:bg-pentaGray dark:text-indigo-200 transition-all duration-100 ease-in ease-out'>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={false}>
+      <div className="bg-[#f5f5f5] text-gray-700 dark:bg-pentaGray dark:text-indigo-200 transition-all duration-100 ease-in ease-out">
         {children}
       </div>
     </ThemeProvider>
