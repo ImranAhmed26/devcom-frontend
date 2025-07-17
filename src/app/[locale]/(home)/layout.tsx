299c60fd18fd14729af839c90399c4c1f2c23eb1
Imran Ahmed <imran.ahk26@gmail.com>
2025-07-17 07:34:57 +0600

    :fire: update layout

--- src/app/[locale]/(home)/layout.tsx
+++ src/app/[locale]/(home)/layout.tsx
@@ -3,9 +3,9 @@ import PrimaryNavbar from "@/components/Navbar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen bg-[#f5f5f5] dark:bg-[#1a2744] dark:text-indigo-200">
+    <div className="flex flex-col min-h-screen bg-[#f5f5f5] dark:bg-[#1a2744] dark:text-indigo-200 overflow-x-clip">
       <PrimaryNavbar />
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center ">
         <div className="mx-auto max-w-[1300px] px-4">{children}</div>
       </div>
       <Footer />
